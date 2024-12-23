6121ae4cc7c3de17c26ad9ca3e2d19a467c53e62
Your Name <bjterry@gmail.com>
2024-12-22 22:06:53 -0500

    chore: Remove welcome message from main function

--- bgbench/main.py
+++ bgbench/main.py
@@ -3,7 +3,6 @@ from bgbench.nim_game import NimGame
 def main():
     game = NimGame()
     print(game.get_rules_explanation())
-    print("Welcome to bgbench!")
 
 if __name__ == "__main__":
     main()

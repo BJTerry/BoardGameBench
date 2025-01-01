b9edcaadedb3406ab4dd31c578802f4f1902bc9f
Your Name <bjterry@gmail.com>
2025-01-01 13:20:21 -0800

    feat: Add Love Letter game to available games list

--- bgbench/games/__init__.py
+++ bgbench/games/__init__.py
@@ -2,12 +2,14 @@ from .nim_game import NimGame
 from .battleship_game import BattleshipGame
 from .war_game import WarGame
 from .guess_who_game import GuessWhoGame
+from .love_letter_game import LoveLetterGame
 
 AVAILABLE_GAMES = {
     'nim': NimGame,
     'battleship': BattleshipGame,
     'war': WarGame,
-    'guess_who': GuessWhoGame
+    'guess_who': GuessWhoGame,
+    'love_letter': LoveLetterGame,
 }
 
 __all__ = ['NimGame', 'BattleshipGame', 'WarGame', 'GuessWhoGame', 'AVAILABLE_GAMES']
